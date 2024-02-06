4533b6af726db15162d29bc3f5904da527a8e5b9
raulc <raulcamacho153@gmail.com>
2024-02-06 12:38:13 +0100

    1 commit

---
+++ Assets/brick.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brick : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        Destroy(gameObject);
+    }
+
+
+}
